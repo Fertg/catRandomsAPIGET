d4cad4ca48416257b2b7539c494af73eca7c0a19
Fertg92 <fertg92@gmail.com>
2023-04-13 11:27:38 +0200

    comentarios en codigo js

--- prueba.js
+++ prueba.js
@@ -1,12 +1,12 @@
-const btn= document.getElementById('btnRandom');
-btn.addEventListener('click',fetchImageShow);
+const btn= document.getElementById('btnRandom'); //recuperamos el botón 
+btn.addEventListener('click',fetchImageShow); //añadimos la funcion click para que al pulsar ejecute la function fetchImageShow
 
 
 function fetchImageShow(){
-    axios.get('https://api.thecatapi.com/v1/images/search?limit=10').then((response)=>{
-        const urlImage=response.data[1].url
+    axios.get('https://api.thecatapi.com/v1/images/search?limit=10').then((response)=>{ //recuperamos la respuesta
+        const urlImage=response.data[1].url //filtramos los datos y al primer resultado
         const content= document.getElementById('content');
-        content.innerHTML=`<img src="${urlImage}"style="max-width: 300px; max-height: 300px;">`;
+        content.innerHTML=`<img src="${urlImage}"style="max-width: 300px; max-height: 300px;">`; //añadimos a la sección
     }).catch(function(error){
         console.log(error);
     });
